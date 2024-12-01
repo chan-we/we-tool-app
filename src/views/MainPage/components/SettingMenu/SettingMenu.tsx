107bec5c315c41f7b517dccfeb9e284cf9496031
chan_we <chan.we@outlook.com>
2024-12-01 21:13:21 +0800

    feat: 检查版本

--- src/views/MainPage/components/SettingMenu/SettingMenu.tsx
+++ src/views/MainPage/components/SettingMenu/SettingMenu.tsx
@@ -1,6 +1,7 @@
 import { SettingOutlined } from '@ant-design/icons'
-import { Dropdown, MenuProps } from 'antd'
+import { Dropdown, MenuProps, Modal } from 'antd'
 import useUpdate from '@/hooks/useUpdate'
+import { getVersion } from '@tauri-apps/api/app'
 
 const SettingMenu = () => {
   const { handleUpdate } = useUpdate()
@@ -10,13 +11,28 @@ const SettingMenu = () => {
       key: 'check-update',
       label: '检查更新',
     },
+    {
+      key: 'version',
+      label: '版本信息',
+    },
   ]
+
+  const handleGetVersion = async () => {
+    const version = await getVersion()
+    // message.info(`当前版本：${version}`)
+    Modal.info({
+      title: '版本信息',
+      content: `当前版本：${version}`,
+    })
+  }
 
   const onClick: MenuProps['onClick'] = ({ key }) => {
     console.log('onClick', key)
     if (key === 'check-update') {
       // emit('tauri://update')
       handleUpdate()
+    } else if (key === 'version') {
+      handleGetVersion()
     }
   }
 
