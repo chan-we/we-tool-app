f67e0b096a016aa4f679086a723b78dd9d62be8e
chan_we <chan.we@outlook.com>
2024-12-01 18:07:13 +0800

    fix: import

--- src/views/MainPage/components/SearchBar/SearchBar.tsx
+++ src/views/MainPage/components/SearchBar/SearchBar.tsx
@@ -2,7 +2,6 @@ import { FolderOpenOutlined } from '@ant-design/icons'
 import { Button, Input } from 'antd'
 import { useState } from 'react'
 import { open } from '@tauri-apps/api/dialog'
-import './index.less'
 
 const SearchBar: React.FC<{
   onChange: (v: string) => void
